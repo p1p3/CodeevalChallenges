e7fa28d188d61d32d9a94d9e7c3e308758690094
Felipe Jaramillo Gómez <pipeluck@gmail.com>
2015-12-12 11:06:36 -0500

    PanGram

---
+++ ChallengesTests/19_BigPositions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeevalChallenges;
+using System.Linq;
+using System;
+
+namespace ChallengesTests
+{
+    [TestClass]
+    public class BigPositionsTest
+    {
+        [TestMethod]
+        public void BigPositionsMainTest()
+        {
+            string path = @"c:\users\pipel\documents\visual studio 2015\Projects\CodeevalChallenges\ChallengesTests\TestFiles\19_BigPositions\Files.txt";
+            _19_BigPositions challenge = new _19_BigPositions(path);
+            bool[] result = (from i in challenge.nlines select i.bitsEqual()).ToArray();
+
+            foreach (_19_BigPositions.nline line in challenge.nlines)
+            {
+                Console.WriteLine(line.bitsEqual().ToString().ToLower());
+            }
+
+            Assert.IsTrue(result[0]);
+            Assert.IsFalse(result[1]);
+        }
+    }
+}
---
+++ ChallengesTests/37_Pangram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeevalChallenges;
+
+namespace ChallengesTests
+{
+    [TestClass]
+    public class _37_PangramTest
+    {
+        [TestMethod]
+        public void PangramMainTest()
+        {
+
+            string text = "A quick brown fox jumps over the lazy dog";
+            _37_Pangrams pangram = new _37_Pangrams(text);
+
+            Assert.AreEqual( 0, pangram.missingLetters().Length);
+
+             text = "A slow yellow fox crawls under the proactive dog";
+             pangram = new _37_Pangrams(text);
+
+            Assert.AreEqual(6,pangram.missingLetters().Length);
+
+        }
+    }
+}
